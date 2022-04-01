6bb809345a25a6c259d5593fada3fc6e174e5d00
Jorge Ariznabarreta - Avanzira <avanzira@outlook.com>
2022-04-01 12:41:00 +0200

    Añado a USER token y rol

--- Models/User.cs
+++ Models/User.cs
@@ -10,6 +10,8 @@ namespace workoholicshop.Models
         public string Email { get; set; }
         public string Password { get; set; }
         public string Adress { get; set; }
+        public string Token { get; set;}
+        public string Rol { get; set; }
 
         public List<Order> Orders { get; set; }
     
